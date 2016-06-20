6ef5d1029b5f79f0237f279a5b4983653510a530
Les Fletcher <les.fletcher@gmail.com>
2016-06-20 10:54:15 -0700

    copy change

--- ruby/011.rb
+++ ruby/011.rb
@@ -21,7 +21,6 @@ STR = "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"
 
-#
 # The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
 #
 # What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
